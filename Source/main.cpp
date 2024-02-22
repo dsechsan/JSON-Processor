44edfff42a153ced057f432d80eccc4ae41ecb24
sandeepchintada <sandeep.chdse@gmail.com>
2024-02-21 19:06:46 -0800

    all tests passing More merge conflicts resolved

--- Source/main.cpp
+++ Source/main.cpp
@@ -11,13 +11,7 @@
 
 // STUDENT: Be sure to update this path if necessary (should point to the repo directory)
 inline std::string getWorkingDirectoryPath() {
-<<<<<<< HEAD
-    //return "C:/UCSD/ECE 141A/Assignments/pa3-json-processor-YourName";
-    return ".";
-=======
     return "/Users/dsechs/Library/CloudStorage/OneDrive-UCSanDiego/Desktop/ECE 141A/PA3";
-//    return ".";
->>>>>>> 34dca01 (Except filtering, all tests pass)
 }
 
 bool runAutoGraderTest(const std::string& aPath, const std::string& aTestName) {
@@ -64,20 +58,19 @@ int main(const int argc, const char* argv[]) {
         return runTest(argc, argv);
 
     // Add your testing code here!
-    using ModelNode = ECE141::ModelNode;
-    ModelNode myNode;
-//    myNode.value = ModelNode::ObjectType{{"key", ModelNode{5.0}}};
+//    using ModelNode = ECE141::ModelNode;
+//    ModelNode myNode;
+////    myNode.value = ModelNode::ObjectType{{"key", ModelNode{5.0}}};
+//
+//    std::string filepath = "/Users/dsechs/Library/CloudStorage/OneDrive-UCSanDiego/Desktop/ECE 141A/PA3/Resources/sammy.json";
+//    std::ifstream inputFile(filepath);
+////    std::istream& inputStream = inputFile;
+//
+//    ECE141::JSONParser jsonParser(inputFile);
+//    ECE141::Model model;
+//    jsonParser.parse(&model);
 
-    std::string filepath = "/Users/dsechs/Library/CloudStorage/OneDrive-UCSanDiego/Desktop/ECE 141A/PA3/Resources/sammy.json";
-    std::ifstream inputFile(filepath);
-//    std::istream& inputStream = inputFile;
 
-    ECE141::JSONParser jsonParser(inputFile);
-    ECE141::Model model;
-    jsonParser.parse(&model);
-
-
-    ECE141::Model model2(model);
 //    auto theQuery = model.createQuery();
 //    auto theResult = theQuery.select("'sammy'.'followers'");
 
@@ -96,6 +89,19 @@ int main(const int argc, const char* argv[]) {
     std::string aTestName = "AdvancedTest";
 
     runAutoGraderTest(aPath,aTestName);
+//    ECE141::Model model2(model);
+//    auto model3 = model2;
+//
+//
+//    auto theQuery = model.createQuery();
+//    auto theResult = theQuery.select("'list'").filter("index < 2").get("*");
+//
+//    if (theResult) std::cout << *theResult << "\n";
+//
+//    std::string aPath = "/Users/dsechs/Library/CloudStorage/OneDrive-UCSanDiego/Desktop/ECE 141A/PA3";
+//    std::string aTestName = "";
+//
+//    runAutoGraderTest(aPath,aTestName);
 
 
 }
